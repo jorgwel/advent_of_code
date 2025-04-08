5dd5df9214c45d40464bc52a0820e8fc8abdcead
Jorge Bautista <jorgwel@gmail.com>
2025-04-08 07:15:10 -0600

    feat: solution to excersice 1 of advent of code

---
+++ advent_of_code/1/solution.rb
@@ -0,0 +1,14 @@
+
+def inverse_captcha(input) 
+  sum = 0
+  input.each_char.with_index do |char, idx|
+      current_index = idx 
+      next_index = (idx + 1) % input.length
+      if input[current_index] == input[next_index]
+        sum += input[current_index].to_i
+      end
+  end
+
+  sum  
+  
+end
