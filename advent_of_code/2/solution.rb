69b2b9e77186eecf2d0e9c57b585c5f80b39dd6c
Jorge Bautista <jorgwel@gmail.com>
2025-04-08 13:46:22 -0600

    feat: adding the operation for checksum of dividable numbers

--- advent_of_code/2/solution.rb
+++ advent_of_code/2/solution.rb
@@ -1,4 +1,22 @@
-def checksum_sum_all_min_max_deltas_in_array(input) 
+def checksum_sum_evenly_dividable_2_numbers_in_row(input) 
+  check_sum = 0
+  input.split("\n").each do |row|
+    if (row.strip() == "") 
+      next
+    end 
+    asc_sorted = collect_row_numbers(row).sort()
+    asc_sorted.reverse().each.with_index do |number, index|
+      asc_sorted.take(asc_sorted.length - index - 1).each do |reversed_list_number| 
+        if (number % reversed_list_number == 0)
+          check_sum += number/reversed_list_number
+        end
+      end
+    end
+  end  
+  check_sum
+end
+
+def checksum_sum_all_min_max_deltas_in_row(input) 
   check_sum = 0
   input.split("\n").each do |row|
     if (row.strip() == "") 
