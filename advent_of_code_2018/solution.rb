00fb6101976b214d34484631a22d9a98f7ae931e
Jorge Bautista <jorgwel@gmail.com>
2025-04-09 13:35:55 -0600

    feat: part 2 of day 1 advent 2018

--- advent_of_code_2018/solution.rb
+++ advent_of_code_2018/solution.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def sum_all_items_in_string(numbers_string)
   sum = 0
   numbers_string.split("\n").each do |line|
@@ -13,4 +15,40 @@ def sum_all_items_in_string(numbers_string)
   sum
 end
 
-#puts "rest_of_line: #{line.strip}"
+def find_first_repeated_calculated_frequency(numbers_string)
+  numbers = turn_string_int_list(numbers_string)
+  frequency_calculations = Set[]
+  repeated_frequency = nil
+  current_frequency = 0
+  while repeated_frequency.nil? 
+    numbers.each do |number|  
+      current_frequency += number
+      
+      if frequency_calculations.include?(current_frequency)
+        repeated_frequency = current_frequency
+        break
+      else
+        
+        frequency_calculations.add(current_frequency)
+      end
+    end
+  end
+  repeated_frequency
+end
+
+def turn_string_int_list(numbers_string)
+  number_list = []
+  numbers_string.split("\n").each do |line|
+    stripped_line = line.strip
+    if stripped_line == "" 
+      next
+    end
+
+    if (stripped_line.chars[0] == '+')
+      number_list << stripped_line[1, line.length].to_i
+    else
+      number_list << stripped_line[1, line.length].to_i * -1
+    end
+  end
+  number_list
+end
